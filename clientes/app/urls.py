e9324b19b00351a767b468e38219f3955618cf0e
CAFE2l <pciskolargx@gmail.com>
2026-02-22 20:36:07 -0300

    error on the conection between local and server

--- clientes/app/urls.py
+++ clientes/app/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include 
+from .views import ClienteCreateView, ClienteListView, ClienteUpdateView
 
 urlpatterns=[
-
+  path("form_cliente", ClienteCreateView.as_view()), 
+  path("lista_clientes", ClienteListView.as_view(), name="lista_clientes"),
+  path("form_cliente/<int:pk>", ClienteUpdateView.as_view()),
 ]
--- clientes/app/views.py
+++ clientes/app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import CreateView,ListView, UpdateView
+from .models import Cliente
 
-# Create your views here.
+class ClienteCreateView(CreateView):
+    model = Cliente
+    fields = "__all__"
+    template_name = "form_cliente.html"
+    success_url = "lista_clientes"
+
+class ClienteListView(ListView):
+    model = Cliente
+    template_name = "lista_clientes.html"
+
+class ClienteUpdateView(UpdateView):
+    model = Cliente
+    fields = "__all__"
+    template_name = "form_cliente.html"
