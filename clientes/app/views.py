5dbcd0c6702a4ca1458c1bd46fb1727ba900b3d8
CAFE2l <pciskolargx@gmail.com>
2026-02-24 15:40:02 -0300

    finished course

--- clientes/app/views.py
+++ clientes/app/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render
 from django.views.generic import CreateView,ListView, UpdateView, DetailView, DeleteView
 from .models import Cliente
 
+
 class ClienteCreateView(CreateView):
     model = Cliente
     fields = "__all__"
     template_name = "form_cliente.html"
     success_url = "lista_clientes"
 
+
 class ClienteListView(ListView):
     model = Cliente
     template_name = "lista_clientes.html"
+
 
 class ClienteUpdateView(UpdateView):
     model = Cliente
@@ -18,10 +21,12 @@ class ClienteUpdateView(UpdateView):
     template_name = "form_cliente.html"
     succes_url = reverse_lazy("lista_clientes")
 
+
 class ClienteDetailView(DetailView):
     model = Cliente
     template_name = "lista_clientes.html"
     context_object_name = "cliente"
+
 
 class ClienteDeleteView(DeleteView):
     model = Cliente
